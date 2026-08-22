17998f016984f6930cb5c8943a2997337f9a41d3
kevinlabens-del <kevin.labens@gmail.com>
2026-08-22 16:18:27 +0200

    Force refresh of deployed PWA cache

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "cr3atix-pong-";
-const CACHE = `${CACHE_PREFIX}v3.0.0`;
+const CACHE = `${CACHE_PREFIX}v3.0.0-r2`;
 const ROOT_URL = new URL("./", self.registration.scope);
 const CORE = [
   ROOT_URL.href,
