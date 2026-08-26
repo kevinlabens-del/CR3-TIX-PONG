cedf8388a52c8dd3268038d7b39673c0df1263af
kevinlabens-del <kevin.labens@gmail.com>
2026-08-26 20:56:31 +0200

    feat: enable touch sensitivity controls

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import InstallSettingsButton from "./InstallSettingsButton";
 import PongV3 from "./PongV3";
+import TouchSensitivityEnhancer from "./TouchSensitivityEnhancer";
 
 export default function Home() {
   return (
     <>
       <PongV3 />
+      <TouchSensitivityEnhancer />
       <InstallSettingsButton />
     </>
   );
