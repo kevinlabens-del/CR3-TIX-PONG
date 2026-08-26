e9596dd0bc5bebfbc76eab7692e11cf6a9f78def
kevinlabens-del <kevin.labens@gmail.com>
2026-08-26 21:22:09 +0200

    perf: enable adaptive mobile performance guard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import InstallSettingsButton from "./InstallSettingsButton";
+import MobilePerformanceGuard from "./MobilePerformanceGuard";
 import PongV3 from "./PongV3";
 import TouchSensitivityEnhancer from "./TouchSensitivityEnhancer";
 
 export default function Home() {
   return (
     <>
+      <MobilePerformanceGuard />
       <PongV3 />
       <TouchSensitivityEnhancer />
       <InstallSettingsButton />
